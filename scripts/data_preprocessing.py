fdbb2fc954616f6e41f6af2b6bf19c56c5a63468
ayushagrawal1139 <aggrawalayush1139@gmail.com>
2025-04-20 14:36:27 +0530

    Modified workflow file

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -49,11 +49,11 @@ def main():
         X_test = scaler.transform(X_test)
         
         # 4. Save processed data
-        np.save('/Users/ayush_agrawal2004/Documents/student_life_generation/lifestyle/daily_life_college_student_project/data/processed/X_train.npy', X_train)
-        np.save('/Users/ayush_agrawal2004/Documents/student_life_generation/lifestyle/daily_life_college_student_project/data/processed/X_test.npy', X_test)
-        np.save('/Users/ayush_agrawal2004/Documents/student_life_generation/lifestyle/daily_life_college_student_project/data/processed/y_train.npy', y_train)
-        np.save('/Users/ayush_agrawal2004/Documents/student_life_generation/lifestyle/daily_life_college_student_project/data/processed/y_test.npy', y_test)
-        joblib.dump(scaler, '/Users/ayush_agrawal2004/Documents/student_life_generation/lifestyle/daily_life_college_student_project/data/processed/scaler.joblib')
+        np.save('data/processed/X_train.npy', X_train)
+        np.save('data/processed/X_test.npy', X_test)
+        np.save('data/processed/y_train.npy', y_train)
+        np.save('data/processed/y_test.npy', y_test)
+        joblib.dump(scaler, 'data/processed/scaler.joblib')
         
         print("Successfully saved processed data:")
         print(os.listdir('data/processed'))
